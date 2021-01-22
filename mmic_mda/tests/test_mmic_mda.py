d12d7cfcdaabf1e0053a23c7d3e23ab1bbbfd917
Andrew-AbiMansour <andrew.gaam@gmail.com>
2021-01-21 19:11:56 -0500

    added guess_bonds to pytest params

--- mmic_mda/tests/test_mmic_mda.py
+++ mmic_mda/tests/test_mmic_mda.py
@@ -10,6 +10,10 @@ import MDAnalysis as mda
 import mmelemental as mm
 
 
+def pytest_generate_tests(metafunc):
+    if "guess_bonds" in metafunc.fixturenames:
+        metafunc.parametrize("guess_bonds", ['False', 'True'])
+
 def test_mmic_mda_imported():
     """Sample test, will always pass so long as import statement worked"""
     assert "mmic_mda" in sys.modules
@@ -17,29 +21,14 @@ def test_mmic_mda_imported():
 
 def test_mda_to_mol(guess_bonds):
     uni = mda.Universe("mmic_mda/data/1dzl_fixed.pdb", guess_bonds=guess_bonds)
-    mda_mol = mmic_mda.models.MdaMolecule(mol=uni)
+    mda_mol = mmic_mda.models.MdaMol(mol=uni)
     mm_mol = mmic_mda.components.MdaToMolComponent.compute(mda_mol)
-    mm_mol.to_file("1dzl_mm.pdb")
+    mm_mol.to_file(f"1dzl_mm_{guess_bonds}.pdb")
 
     return mm_mol
 
-
-def test_mol_to_mda():
-    with mm.models.util.output.FileOutput(path="1dzl_mm.pdb", clean=True) as fo:
-        mm_mol = mm.models.molecule.mm_molecule.Molecule.from_file(fo.abs_path)
+def test_mol_to_mda(guess_bonds):
+    with mm.models.util.output.FileOutput(path=f"1dzl_mm_{guess_bonds}.pdb", clean=True) as fo:
+        mm_mol = mm.models.molecule.mm_mol.Mol.from_file(fo.abs_path)
 
     return mmic_mda.components.MolToMdaComponent.compute(mm_mol)
-
-
-mm_mol = test_mda_to_mol(guess_bonds=False)
-mda_mol = test_mol_to_mda()
-
-print(mm_mol)
-print(mda_mol)
-print(mm_mol.geometry)
-
-mm_mol = test_mda_to_mol(guess_bonds=True)
-mda_mol = test_mol_to_mda()
-
-print(mm_mol)
-print(mda_mol)
