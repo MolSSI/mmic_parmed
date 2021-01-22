2e05bbbf7fdc079675ea7bdc01962c103c26e7ac
Andrew-AbiMansour <andrew.gaam@gmail.com>
2021-01-22 16:16:54 -0500

    added test for data conversions

--- mmic_mda/tests/test_mmic_mda.py
+++ mmic_mda/tests/test_mmic_mda.py
@@ -12,7 +12,8 @@ import mmelemental as mm
 
 def pytest_generate_tests(metafunc):
     if "guess_bonds" in metafunc.fixturenames:
-        metafunc.parametrize("guess_bonds", ['False', 'True'])
+        metafunc.parametrize("guess_bonds", ["False", "True"])
+
 
 def test_mmic_mda_imported():
     """Sample test, will always pass so long as import statement worked"""
@@ -23,12 +24,21 @@ def test_mda_to_mol(guess_bonds):
     uni = mda.Universe("mmic_mda/data/1dzl_fixed.pdb", guess_bonds=guess_bonds)
     mda_mol = mmic_mda.models.MdaMol(mol=uni)
     mm_mol = mmic_mda.components.MdaToMolComponent.compute(mda_mol)
-    mm_mol.to_file(f"1dzl_mm_{guess_bonds}.pdb")
 
     return mm_mol
 
+
 def test_mol_to_mda(guess_bonds):
-    with mm.models.util.output.FileOutput(path=f"1dzl_mm_{guess_bonds}.pdb", clean=True) as fo:
-        mm_mol = mm.models.molecule.mm_mol.Mol.from_file(fo.abs_path)
+    mm_mol = mm.models.molecule.mm_mol.Mol.from_file("mmic_mda/data/1dzl_fixed.pdb")
 
     return mmic_mda.components.MolToMdaComponent.compute(mm_mol)
+
+
+def test_io_methods(guess_bonds):
+    mda_mol = mmic_mda.models.MdaMol.from_file(
+        "mmic_mda/data/1dzl_fixed.pdb", guess_bonds=guess_bonds
+    )
+    assert isinstance(mda_mol.mol, mda_mol.dtype)
+
+    mm_mol = mda_mol.to_data()
+    assert isinstance(mm_mol, mm.models.molecule.Mol)
