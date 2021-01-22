2efe8993c9d8b9b5575a63a25b7f7c72b3a2c68f
Andrew-AbiMansour <andrew.gaam@gmail.com>
2021-01-22 16:17:25 -0500

    added ext maps

--- mmic_mda/models/__init__.py
+++ mmic_mda/models/__init__.py
@@ -1,1 +1,15 @@
 from .mol import MdaMol
+
+molread_ext_maps = {
+    ".gro": "gro",
+    ".pdb": "pdb",
+    ".top": "top",
+    ".psf": "psf",
+}
+
+molwrite_ext_maps = {
+    ".gro": "gro",
+    ".pdb": "pdb",
+    ".top": "top",
+    ".psf": "psf",
+}
