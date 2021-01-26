6682365b8a5b38c05099b545d687a676884659aa
Andrew-AbiMansour <andrew.gaam@gmail.com>
2021-01-26 12:01:51 -0500

    updated write ext_map

--- mmic_mda/models/__init__.py
+++ mmic_mda/models/__init__.py
@@ -1,6 +1,7 @@
 from .mol import *
 from .traj import *
 
+# Need to update these lists
 molread_ext_maps = {
     ".gro": "gro",
     ".pdb": "pdb",
@@ -8,11 +9,6 @@ molread_ext_maps = {
     ".psf": "psf",
 }
 
-molwrite_ext_maps = {
-    ".gro": "gro",
-    ".pdb": "pdb",
-    ".top": "top",
-    ".psf": "psf",
-}
+molwrite_ext_maps = {".gro": "gro", ".pdb": "pdb"}
 
 classes_map = {"Mol": MdaMol, "Traj": MdaTraj}
