35be912165aca6785f3671fa33861e4b3baa940c
Andrew-AbiMansour <andrew.gaam@gmail.com>
2021-01-30 23:07:12 -0500

    shifted maps from models.__init__ to pkg.__init__

--- mmic_mda/__init__.py
+++ mmic_mda/__init__.py
@@ -13,3 +13,26 @@ versions = get_versions()
 __version__ = versions["version"]
 __git_revision__ = versions["full-revisionid"]
 del get_versions, versions
+
+# Need to update these lists
+molread_ext_maps = {
+    ".gro": "gro",
+    ".pdb": "pdb",
+    ".top": "top",
+    ".psf": "psf",
+}
+
+molwrite_ext_maps = {".gro": "gro", ".pdb": "pdb"}
+
+units = {
+    "length": "angstrom",
+    "time": "ps",
+    "energy": "kJ/mol",
+    "charge": "e",
+    "speed": "angstrom/ps",
+    "force": "kJ/(mol*angstrom)",
+    "mass": "amu",
+    "angle": "degrees",
+}
+
+_classes_map = {"Mol": models.MdaMol, "Traj": models.MdaTraj}
--- mmic_mda/models/__init__.py
+++ mmic_mda/models/__init__.py
@@ -1,14 +1,2 @@
 from .mol import *
 from .traj import *
-
-# Need to update these lists
-molread_ext_maps = {
-    ".gro": "gro",
-    ".pdb": "pdb",
-    ".top": "top",
-    ".psf": "psf",
-}
-
-molwrite_ext_maps = {".gro": "gro", ".pdb": "pdb"}
-
-classes_map = {"Mol": MdaMol, "Traj": MdaTraj}
