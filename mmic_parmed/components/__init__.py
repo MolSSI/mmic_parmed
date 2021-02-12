8c435dba2ad7453a1b9d70dd4c9585e7000522e7
Andrew-AbiMansour <andrew.gaam@gmail.com>
2021-02-12 01:04:50 -0500

    added ff_component

--- mmic_parmed/components/__init__.py
+++ mmic_parmed/components/__init__.py
@@ -1,3 +1,4 @@
-from . import mol_component, traj_component
+from . import mol_component, traj_component, ff_component
 from .mol_component import *
 from .traj_component import *
+from .ff_component import *
