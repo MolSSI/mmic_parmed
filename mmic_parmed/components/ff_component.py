f985eba01604e89d2d29e2b54859aae5f75b8470
Andrew-AbiMansour <andrew.gaam@gmail.com>
2021-03-12 13:36:21 -0500

    add mmic_translator as a dep

--- mmic_parmed/components/ff_component.py
+++ mmic_parmed/components/ff_component.py
@@ -1,23 +1,21 @@
 from mmelemental.models.util.output import FileOutput
-from mmelemental.models.forcefield.mm_ff import (
-    ForceField,
-    Bonds,
-    Angles,
-    Dihedrals,
-    NonBonded,
-)
-from mmic_translator.models.ff_io import (
-    FFInToSchema,
-    FFInFromSchema,
-    FFOutToSchema,
-    FFOutFromSchema,
+from mmelemental.models import forcefield
+from mmic_translator.models.io import (
+    TransInput,
+    TransOutput,
 )
 from mmic_translator.components import TransComponent
 from typing import Dict, Any, List, Tuple, Optional
 from mmelemental.util.units import convert
 import parmed
+from enum import Enum
 
 __all__ = ["FFToParmedComponent", "ParmedToFFComponent"]
+
+bondTypes = {
+    1: forcefield.bonded.bonds.potentials.Harmonic,
+    2: forcefield.bonded.bonds.potentials.Gromos96,
+}
 
 
 class FFToParmedComponent(TransComponent):
@@ -25,24 +23,27 @@ class FFToParmedComponent(TransComponent):
 
     @classmethod
     def input(cls):
-        return FFInFromSchema
+        return TransInput
 
     @classmethod
     def output(cls):
-        return FFOutFromSchema
+        return TransOutput
 
     def execute(
         self,
-        inputs: FFInFromSchema,
+        inputs: TransInput,
         extra_outfiles: Optional[List[str]] = None,
         extra_commands: Optional[List[str]] = None,
         scratch_name: Optional[str] = None,
         timeout: Optional[int] = None,
-    ) -> Tuple[bool, FFOutFromSchema]:
+    ) -> Tuple[bool, TransOutput]:
         """
         Works for writing PDB files e.g. pmol.save("file.pdb") but fails for gro files
         TODO: need to investigate this more. Routine is also very slow. Try to vectorize.
         """
+        if isinstance(inputs, dict):
+            inputs = self.input()(**inputs)
+
         ff = parmed.structure.Structure()
         params = parmed.parameters.ParameterSet()
 
@@ -52,7 +53,7 @@ class FFToParmedComponent(TransComponent):
         natoms = len(mm_ff.symbols)
         masses = mm_ff.masses
 
-        rmin_factor = convert(1.0, atom.urmin.unit.get_name(), mm_ff.bonds.length_units)
+        rmin_factor = convert(1.0, atom.urmin.unit.get_name(), mm_ff.bonds.lengths_units)
         rmin_14_factor = convert(
             1.0, atom.urmin_14.unit.get_name(), mm_ff.nonbonded.sigma_units
         )
@@ -108,7 +109,7 @@ class FFToParmedComponent(TransComponent):
             # classparmed.Residue(name, number=- 1, chain='', insertion_code='', segid='', list=None)[source]
             pmol.add_atom(atom, resname, resnum + 1, chain="", inscode="", segid="")
 
-        return True, FFOutFromSchema(tk_object=pmol, units=units)
+        return True, TransOutput(tk_object=pmol, units=units)
 
 
 class ParmedToFFComponent(TransComponent):
@@ -116,48 +117,56 @@ class ParmedToFFComponent(TransComponent):
 
     @classmethod
     def input(cls):
-        return FFInToSchema
+        return TransInput
 
     @classmethod
     def output(cls):
-        return FFOutToSchema
+        return TransOutput
 
     def execute(
         self,
-        inputs: FFInToSchema,
+        inputs: TransInput,
         extra_outfiles: Optional[List[str]] = None,
         extra_commands: Optional[List[str]] = None,
         scratch_name: Optional[str] = None,
         timeout: Optional[int] = None,
-    ) -> Tuple[bool, FFOutToSchema]:
+    ) -> Tuple[bool, TransOutput]:
 
         if isinstance(inputs, dict):
             inputs = self.input()(**inputs)
 
         ff = inputs.tk_object
-        mm_ff = ForceField(
-            nonbonded=[], bonds=[], angles=[]
-        )  # empty FF object, provides units
+        mm_units = forcefield.ForceField.get_units()
+
+        # Need to map these to potential types
+        bonds_units = forcefield.bonded.bonds.potentials.harmonic.Harmonic.get_units()
+        angles_units = forcefield.bonded.angles.potentials.harmonic.Harmonic.get_units()
+        dihedrals_units = forcefield.bonded.dihedrals.potentials.harmonic.Harmonic.get_units()
+        lj_units = forcefield.nonbonded.potentials.lenjones.LennardJones.get_units()
+
         atom = ff.atoms[0]
         bond = ff.bonds[0] if len(ff.bonds) else None
         angle = ff.angles[0] if len(ff.angles) else None
         dihedral = ff.dihedrals[0] if len(ff.dihedrals) else None
 
-        data = [(atom.name, atom.type, atom.charge) for atom in ff.atoms]
-        names, symbols, charges = zip(*data)
+        data = [(atom.name, atom.type, atom.charge, atom.mass) for atom in ff.atoms]
+        names, symbols, charges, masses = zip(*data)
         charges = convert(
-            charges, atom.ucharge.unit.get_symbol(), mm_ff.nonbonded.charges_units
+            charges, atom.ucharge.unit.get_symbol(), mm_units["charges_units"]
+        )
+        masses = convert(
+            masses, atom.umass.unit.get_symbol(), mm_units["masses_units"]
         )
 
-        rmin_factor = convert(1.0, atom.urmin.unit.get_name(), mm_ff.bonds.length_units)
+        rmin_factor = convert(1.0, atom.urmin.unit.get_name(), bonds_units["lengths_units"])
         rmin_14_factor = convert(
-            1.0, atom.urmin_14.unit.get_name(), mm_ff.nonbonded.sigma_units
+            1.0, atom.urmin_14.unit.get_name(), lj_units["sigma_units"]
         )
         uepsilon_factor = convert(
-            1.0, atom.uepsilon.unit.get_name(), mm_ff.nonbonded.epsilon_units
+            1.0, atom.uepsilon.unit.get_name(), lj_units["epsilon_units"]
         )
         epsilon_14_factor = convert(
-            1.0, atom.uepsilon_14.unit.get_name(), mm_ff.nonbonded.epsilon_units
+            1.0, atom.uepsilon_14.unit.get_name(), lj_units["epsilon_units"]
         )
         scaling_factor = 1.0 / 2 ** (1.0 / 6.0)  # rmin = 2^(1/6) sigma
         params = [
@@ -171,39 +180,55 @@ class ParmedToFFComponent(TransComponent):
         ]
         sigma, sigma_14, epsilon, epsilon_14 = zip(*params)
 
-        nonbonded = NonBonded(sigma=sigma, epsilon=epsilon, charges=charges, form="LJ")
+        lj = forcefield.nonbonded.potentials.LennardJones(sigma=sigma, epsilon=epsilon)
+        # Need to include sigma_14 and epsilon_14
+        nonbonded = forcefield.nonbonded.NonBonded(params=lj)
 
         if bond:
             bond_req_factor = convert(
-                1.0, bond.type.ureq.unit.get_name(), mm_ff.bonds.length_units
+                1.0, bond.type.ureq.unit.get_name(), bonds_units["lengths_units"]
             )
             bond_k_factor = convert(
-                1.0, bond.type.uk.unit.get_name(), mm_ff.bonds.spring_units
+                1.0, bond.type.uk.unit.get_name(), bonds_units["spring_units"]
             )
             bonds_lengths = [bond.type.req * bond_req_factor for bond in ff.bonds]
             bonds_k = [bond.type.k * bond_k_factor for bond in ff.bonds]
             bonds_type = [bond.funct for bond in ff.bonds]
 
-            bonds = Bonds(
-                length=bonds_lengths, spring=bonds_k, form=str(bonds_type)
-            )  # need to work out the form
+            unique_bonds_type = set(bonds_type)
+            if len(unique_bonds_type) > 1:
+                params = [
+                    bondTypes.get(btype)(spring=bonds_k[bonds_type == btype])
+                    for btype in unique_bonds_type if bondTypes.get(btype)
+                ]
+            else:
+                params = forcefield.bonded.bonds.potentials.Harmonic(spring=bonds_k, lengths=bonds_lengths)
+
+            bonds = forcefield.bonded.Bonds(params=params)
         else:
             bonds = None
 
         if angle:
             angle_theta_factor = convert(
-                1.0, angle.type.utheteq.unit.get_name(), mm_ff.angles.angle_units
+                1.0, angle.type.utheteq.unit.get_name(), angles_units["angles_units"]
             )
             angle_k_factor = convert(
-                1.0, angle.type.uk.unit.get_name(), mm_ff.angles.spring_units
+                1.0, angle.type.uk.unit.get_name(), angles_units["spring_units"]
             )
             angles_ = [angle.type.theteq for angle in ff.angles]
             angles_k = [angle.type.k for angle in ff.angles]
             angles_type = [angle.funct for angle in ff.angles]
 
-            angles = Angles(
-                angle=angles_, spring=angles_k, form=str(angles_type)
-            )  # need to work out the form
+            unique_angles_type = set(angles_type)
+            if len(unique_angles_type) > 1:
+                params = [
+                    angleTypes.get(btype)(spring=angles_k[angles_type == btype])
+                    for btype in unique_angles_type if angleTypes.get(btype)
+                ]
+            else:
+                params = forcefield.bonded.angles.potentials.Harmonic(spring=angles_k, angles=angles_)
+
+            angles = forcefield.bonded.Angles(params=params)
         else:
             angles = None
 
@@ -215,10 +240,10 @@ class ParmedToFFComponent(TransComponent):
             )
 
             dihedral_phi_factor = convert(
-                1.0, dtype.uphi_k.unit.get_name(), mm_ff.dihedrals.phi_units
+                1.0, dtype.uphi_k.unit.get_name(), dihedrals_units["angles_units"]
             )
             dihedral_phase_factor = convert(
-                1.0, dtype.uphase.unit.get_name(), mm_ff.dihedrals.phase_units
+                1.0, dtype.uphase.unit.get_name(), dihedrals_units["phases_units"]
             )
 
             # Need to look into per, scee, and scnb ... their meaning, units, etc.
@@ -247,6 +272,8 @@ class ParmedToFFComponent(TransComponent):
         inclusions = None
 
         input_dict = {
+            "masses": masses,
+            "charges": charges,
             "bonds": bonds,
             "angles": angles,
             "dihedrals": None,
@@ -258,4 +285,4 @@ class ParmedToFFComponent(TransComponent):
             "symbols": symbols,
         }
 
-        return True, FFOutToSchema(schema_object=ForceField(**input_dict))
+        return True, TransOutput(schema_object=forcefield.ForceField(**input_dict))
