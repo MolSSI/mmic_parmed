778b0629484cefe5337fcd5f9722f30a830b311e
Andrew-AbiMansour <andrew.gaam@gmail.com>
2021-03-20 04:10:44 -0400

    add test function for FFtoParmedComponent

--- mmic_parmed/tests/test_mmic_parmed_ff.py
+++ mmic_parmed/tests/test_mmic_parmed_ff.py
@@ -12,12 +12,16 @@ import mmelemental as mm
 
 
 data_dir = os.path.join("mmic_parmed", "data")
-top_file = lambda ext: os.path.join(data_dir, "forcefields", f"1dzl_gro.{ext}")
+top_files = lambda ext: os.path.join(data_dir, "forcefields", f"1dzl_gro.{ext}")
+json_files = os.path.join(data_dir, "forcefields", "forcefield-single.json")
 
 
 def pytest_generate_tests(metafunc):
-    if "file" in metafunc.fixturenames:
-        metafunc.parametrize("file", [top_file("top")])
+    if "top_file" in metafunc.fixturenames:
+        metafunc.parametrize("top_file", [top_files("top")])
+
+    if "json_file" in metafunc.fixturenames:
+        metafunc.parametrize("json_file", [json_files])
 
 
 def test_mmic_parmed_imported():
@@ -25,20 +29,20 @@ def test_mmic_parmed_imported():
     assert "mmic_parmed" in sys.modules
 
 
-def test_parmed_to_ff(file, **kwargs):
-    ff = parmed.load_file(file)
+def test_parmed_to_ff(top_file, **kwargs):
+    ff = parmed.load_file(top_file)
     inputs = {"tk_object": ff, "kwargs": kwargs}
     return mmic_parmed.components.ParmedToFFComponent.compute(inputs)
 
-@pytest.mark.skip("FFToParmedComponent still broken.")
-def test_ff_to_parmed(file, **kwargs):
-    mm_ff = mm.models.forcefield.mm_ff.ForceField.from_file(file)
+
+def test_ff_to_parmed(json_file, **kwargs):
+    mm_ff = mm.models.forcefield.mm_ff.ForceField.from_file(json_file)
     inputs = {"schema_object": mm_ff, "kwargs": kwargs}
     return mmic_parmed.components.FFToParmedComponent.compute(inputs)
 
 
-def test_io_methods(file):
-    pff = mmic_parmed.models.ParmedFF.from_file(file)
+def test_io_methods(top_file):
+    pff = mmic_parmed.models.ParmedFF.from_file(top_file)
     assert isinstance(pff.data, pff.dtype)
 
     pff.to_file("ff.psf")
