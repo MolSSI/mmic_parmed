60a08bdd99fb48afe636ec2598fa6dbf782ff69e
Andrew-AbiMansour <andrew.gaam@gmail.com>
2021-09-15 15:23:53 -0400

    move file_ext maps and version to mmic_parmed.py

--- mmic_parmed/__init__.py
+++ mmic_parmed/__init__.py
@@ -5,53 +5,17 @@ A short description of the project.
 
 # Add imports here
 from . import components, models
+from .mmic_parmed import (
+    molread_ext_maps,
+    molwrite_ext_maps,
+    ffread_ext_maps,
+    ffwrite_ext_maps,
+    __version__,
+)
 
-# Handle versioneer
-from ._version import get_versions
-
-versions = get_versions()
-__version__ = versions["version"]
-__git_revision__ = versions["full-revisionid"]
-del get_versions, versions
-
-# Need to update these lists
-molread_ext_maps = {
-    ".gro": "gro",
-    ".psf": "psf",
-    ".pdb": "pdb",
-    ".sdf": "sdf",
-    ".mol": "mol",
-    ".mol2": "mol2",
-    ".inpcrd": "inpcrd",
-}
-
-molwrite_ext_maps = {
-    ".gro": "gro",
-    ".pdb": "pdb",
-    ".inpcrd": "inpcrd",
-}
-
-ffread_ext_maps = {
-    ".psf": "psf",
-    ".top": "top",
-    ".prm": "prm",
-    ".prmtop": "prmtop",
-}
-ffwrite_ext_maps = {
-    ".psf": "psf",
-    ".top": "top",
-    ".prm": "prm",
-    ".prmtop": "prmtop",
-}
 
 _classes_map = {
     "Molecule": models.ParmedMol,
     # "Trajectory": models.ParmedTraj,
     "ForceField": models.ParmedFF,
-}
-
-provenance_stamp = {
-    "creator": "mmic_parmed",
-    "version": __version__,
-    "routine": __name__,
 }
